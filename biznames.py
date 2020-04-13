e679e5a5bd09f791bfd34292b3cfddf993451560
Kalovelo <apostoloskalovelonis@gmail.com>
2020-04-13 11:20:56 +0300

    Use Commandline Args

--- biznames.py
+++ biznames.py
@@ -1,14 +1,11 @@
-#!/usr/bin/python
 import re
 import sys
+import getopt
 
 
 def prepareName(fullname):
     nameParts = fullname.split(" ")
     return nameParts[0], nameParts[1]
-
-
-biznames = []
 
 
 def computeName(first, last, limit=None,):
@@ -29,10 +26,58 @@ def computeName(first, last, limit=None,):
     return nameList
 
 
-with open('fullnames.txt') as readFile:
-    with open('biznames.txt', 'w') as out:
-        for fullname in readFile.read().splitlines():
-            name, lastname = prepareName(fullname)
-            for bizname in computeName(name, lastname):
-                if(bizname):
-                    out.write(f'\n{bizname}')
+def readWriteBizname(output, namefile, limit=None):
+    with open(namefile) as readFile:
+        with open(output, 'w') as out:
+            for fullname in readFile.read().splitlines():
+                name, lastname = prepareName(fullname)
+                for bizname in computeName(name, lastname, limit):
+                    if(bizname):
+                        out.write(f'\n{bizname}')
+
+
+def WriteBizname(output, fullname, limit=None):
+    with open(output, 'w') as out:
+        name, lastname = prepareName(fullname)
+        for bizname in computeName(name, lastname, limit):
+            if(bizname):
+                out.write(f'\n{bizname}')
+
+
+def main(argv):
+    namefile = ''
+    output = 'biznames.txt'
+    singleName = ''
+    limit = None
+    try:
+        opts, _ = getopt.getopt(
+            argv, "l:n:o:w:", ["limit=", "name=", "output=", "wordlist="])
+        for opt, arg in opts:
+            if opt == "-o":
+                output = arg
+            if opt == "-l":
+                limit = int(arg)
+            if opt == "-w":
+                namefile = arg
+            if opt == "-n":
+                singleName = arg
+
+        if(singleName):
+            WriteBizname(output, singleName, limit)
+        else:
+            readWriteBizname(output, namefile, limit)
+    except Exception as error:
+        print(error)
+        print("==== BUSINESS NAME GENERATOR ====")
+        print("[*] Args List [*]")
+        print("|| -o output file")
+        print("|| -w wordlist/namelist file")
+        print("|| -n single name generate")
+        print("|| -l username char limit")
+        print("[*] use :")
+        print("|| > python3 biznames.py -w fullnames.txt -o biznames.txt")
+        print("|| > python3 biznames.py -n 'Niko Bellic' -o biznames.txt")
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
